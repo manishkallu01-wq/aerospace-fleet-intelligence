22eed5c82bd3e0319a1ea0e9180db85b31e5166d
Manish Kallu <manishkallu01@gmail.com>
2026-08-18 16:52:09 -0700

    fix: align dbt staging model with Gold schema

--- dbt/models/staging/stg_engine_health.sql
+++ dbt/models/staging/stg_engine_health.sql
@@ -3,9 +3,8 @@
 select
     cast(engine_id as integer) as engine_id,
     cast(cycle as integer) as cycle,
-    cast(rul as integer) as rul,
     cast(sensor_mean as double) as sensor_mean,
-    cast(sensor_instability as double) as sensor_instability,
-    cast(health_score as double) as health_score,
+    cast(sensor_stddev as double) as sensor_stddev,
+    cast(condition_age_proxy as double) as condition_age_proxy,
     cast(risk_band as varchar) as risk_band
-from {{ source('aerospace', 'engine_health') }}
+from {{ source('aerospace', 'fct_engine_health') }}
