22f953966c1a3208f59d40be4e605a45f9bb9f18
Manish Kallu <manishkallu01@gmail.com>
2026-08-18 16:11:52 -0700

    feat: add maintenance opportunity mart

---
+++ dbt/models/marts/fct_maintenance_opportunity.sql
@@ -0,0 +1,15 @@
+{{ config(materialized='table') }}
+
+select
+    engine_id,
+    cycle,
+    rul,
+    health_score,
+    risk_band,
+    case
+        when risk_band = 'CRITICAL' then 'Immediate review'
+        when risk_band = 'HIGH' then 'Schedule intervention'
+        when risk_band = 'WATCH' then 'Increase monitoring'
+        else 'Routine monitoring'
+    end as recommended_action
+from {{ ref('stg_engine_health') }}
